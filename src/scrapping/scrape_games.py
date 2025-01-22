ad4069a2cb92ae02bb3c9a7c1dc636d2ad0e10e0
victor <ortinsv1@gmail.com>
2025-01-22 11:19:33 -0300

    Original script

--- src/scrapping/scrape_games.py
+++ src/scrapping/scrape_games.py
@@ -6,8 +6,5 @@ if __name__ == '__main__':
     urls_scrapper = TournamentUrlsScrapper(base_url='http://www.salaooval.com.br/campeonatos/', category='masculino')
     urls = urls_scrapper.get_urls()
     
-    games_scrapper = GamesScrapper(urls, save_path='games5.csv')
-    # Já tá no número certo!!! Lembra de apagar essa msg qnd executar dnv
-    all_games = games_scrapper.scrape_tournaments(init=47, end=len(urls), verbose=True)
-
-    all_games.to_csv('games6.csv')
+    games_scrapper = GamesScrapper(urls, save_path='data/games.csv')
+    all_games = games_scrapper.scrape_tournaments(init=0, end=len(urls), verbose=True)
