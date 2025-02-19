0ccbf987173e1b44fe3f200b8311c0901f645d6e
victor <ortinsv1@gmail.com>
2025-02-18 21:11:20 -0300

    changing vars names

--- src/scrapping/scrape_teams/scrape_teams.py
+++ src/scrapping/scrape_teams/scrape_teams.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     urls_scrapper = TeamUrlsScrapper(base_url='https://www.salaooval.com.br/times/')
     urls = urls_scrapper.get_urls()
     
-    games_scrapper = TeamsScrapper(urls, save_path='data/teams.csv')
-    all_games = games_scrapper.scrape_teams(init=0, end=len(urls), verbose=True)
+    teams_scrapper = TeamsScrapper(urls, save_path='data/teams.csv')
+    all_games = teams_scrapper.scrape_teams(init=0, end=len(urls), verbose=True)
