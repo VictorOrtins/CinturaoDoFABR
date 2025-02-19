83350be7aab3dde0668e8d8fa73aaf22ab98f965
victor <ortinsv1@gmail.com>
2025-02-18 21:14:27 -0300

    Adding cached methods and function to get url

--- app/utils/data_utils.py
+++ app/utils/data_utils.py
@@ -1,10 +1,12 @@
 import requests
 
 import pandas as pd
+import streamlit as st
 
 from PIL import Image
 
 
+@st.cache_data
 def read_csv_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
@@ -15,5 +17,13 @@ def resize_img(image: Image, new_height: int) -> Image:
     height, width = int(image.size[0]), int(image.size[1])
     new_width = int(new_height * width/height)
     return image.resize((new_width, new_height), Image.LANCZOS)
+
+def get_img_url(time, teams_df):
+    try:
+        team_df = teams_df[teams_df['Nome'] == time].iloc[0]
+    except Exception:
+        return ''
+    
+    return team_df['URL da Imagem']
 
     
